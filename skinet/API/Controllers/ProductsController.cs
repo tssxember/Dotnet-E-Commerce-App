12837dc72febee879b2dea4c47926b3df833119b
Vincent <vincent.fezzuoglio@protonmail.com>
2024-02-05 15:43:03 -0500

    commiting changes

---
+++ skinet/API/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "This is a list of products!";
+        }
+
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return "This is a prduct";
+        }
+    }
+}
